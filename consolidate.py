7ee7d343e80484687156bc65ae6b4276a81c2abe
mehran <mehran@in-school.net>
2016-03-30 14:04:49 -0400

    enabled prettyprinting and saving of consolidated data

--- consolidate.py
+++ consolidate.py
@@ -10,8 +10,9 @@ class consolidate():
 		self.result[aProc]['memory_MB'] = sum([aTup[1] for aTup in self.dump[aProc]['memory_info']])/len(self.dump[aProc]['memory_info'])
 		self.result[aProc]['cpu_percent'] = sum([aTup[1] for aTup in self.dump[aProc]['cpu_info']])/len(self.dump[aProc]['cpu_info'])
 		self.result[aProc]['net_load'] = {'up':None, 'down':None}
-		self.result[aProc]['net_load']['up'] = sum( [aTup[1] for aTup in self.dump[aProc]['net_load']['up']] )/len(self.dump[aProc]['net_load']['up'])
-		self.result[aProc]['net_load']['down'] = sum( [aTup[1] for aTup in self.dump[aProc]['net_load']['down']] )/len(self.dump[aProc]['net_load']['down'])
+		if len(self.dump[aProc]['net_load']['up'])>0:
+			self.result[aProc]['net_load']['up'] = sum( [aTup[1] for aTup in self.dump[aProc]['net_load']['up']] )/len(self.dump[aProc]['net_load']['up'])
+			self.result[aProc]['net_load']['down'] = sum( [aTup[1] for aTup in self.dump[aProc]['net_load']['down']] )/len(self.dump[aProc]['net_load']['down'])
 
 	def summary(self):
 		for aProc in self.dump:	self.consolidate(aProc)
@@ -21,4 +22,10 @@ if __name__ == '__main__':
 	if sys.argv[1:]:
 		with open(sys.argv[1],'r') as f: dump = json.loads(f.read())
 		c = consolidate(dump)
-		print(c.summary())
+		#print(c.summary())
+		with open(sys.argv[1] + '_consolidated.json', 'w') as fw: 
+			jd = json.dumps(c.summary(), indent=4)
+			fw.write(jd)
+			print(jd)
+
+		
